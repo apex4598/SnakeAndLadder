54d800b8340e31b927ecc3ff60dec39d30344b39
Apex4598 <login2ajit@gmail.com>
2022-05-19 21:05:12 +0530

    Check For Options

--- src/com/bridgelabz/basic/SnakeAndLadder.java
+++ src/com/bridgelabz/basic/SnakeAndLadder.java
@@ -1,11 +1,35 @@
 package com.bridgelabz.basic;
 
 public class SnakeAndLadder {
+        public static final int NO_PLAY = 0;
+        public static final int LADDER = 1;
+        public static final int SNAKE = 2;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder game.");
 
         int startPosition=0;
+        int Position=0;
+        int newPosition=0;
+
         int DieRolled = (int) (Math.random()*6)+1;
-        System.out.println(DieRolled);
+        int checkOption = (int) (Math.random()*3);
+        //System.out.println(DieRolled);
+
+        switch (checkOption) {
+            case NO_PLAY:
+                newPosition = 0;
+                break;
+            case LADDER:
+                newPosition = DieRolled;
+                break;
+            case SNAKE:
+                newPosition = -DieRolled;
+        }
+        Position=Position+newPosition;
+        if (Position<0){
+                Position = 0;
+        }
+        System.out.println("You are now at "+Position+"th"+" position");
     }
 }
